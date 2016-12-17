cc825d08b1ef6f53d112a693627104d839185acc
zutrich@gmail.com <st310232a>
2016-12-17 16:10:51 +0300

    JsonUtils nullableJSONArray and nullableJSONObject functions

--- kae-util/src/main/java/kae/util/JsonUtils.java
+++ kae-util/src/main/java/kae/util/JsonUtils.java
@@ -7,6 +7,7 @@
 
 package kae.util;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -19,6 +20,22 @@ import java.util.Date;
  */
 public class JsonUtils {
 
+  public static JSONObject nullableJSONObject(JSONObject o, String key) throws JSONException {
+    if (o.has(key) && !o.isNull(key)) {
+      return o.getJSONObject(key);
+    } else {
+      return null;
+    }
+  }
+
+  public static JSONArray nullableJSONArray(JSONObject o, String key) throws JSONException {
+    if (o.has(key) && !o.isNull(key)) {
+      return o.getJSONArray(key);
+    } else {
+      return null;
+    }
+  }
+
   public static String getString(JSONObject o, String key) throws JSONException {
     if (o.has(key) && !o.isNull(key)) {
       return o.getString(key);
@@ -29,7 +46,27 @@ public class JsonUtils {
 
   public static int getInt(JSONObject o, String key) throws JSONException {
     if (o.has(key) && !o.isNull(key)) {
-      return o.getInt(key);
+      Object object = o.get(key);
+
+      try {
+        return object instanceof Number ? ((Number) object).intValue() : Integer.parseInt((String) object);
+      } catch (NumberFormatException e) {
+        return 0;
+      }
+    } else {
+      return 0;
+    }
+  }
+
+  public static float getFloat(JSONObject o, String key) throws JSONException {
+    if (o.has(key) && !o.isNull(key)) {
+      Object object = o.get(key);
+
+      try {
+        return object instanceof Number ? ((Number) object).floatValue() : Float.parseFloat((String) object);
+      } catch (NumberFormatException e) {
+        return 0;
+      }
     } else {
       return 0;
     }
